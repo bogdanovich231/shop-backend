2129197d9ec80fd41e8df2f95f9537d4bc1754c0
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-03-06 01:26:28 +0100

    feat: create new bucket & lambda function import & created new signed url

---
+++ import-service/handlers/utils/headers.ts
@@ -0,0 +1,9 @@
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Credentials": true,
+  "Access-Control-Allow-Methods": "GET",
+};
+
+export default headers;
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,62 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "ImportBucket", {
+      bucketName: "import-service-bucket-rs1",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaRole = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+
+    bucket.grantPut(lambdaRole);
+
+    const importProductsFile = new lambda.Function(
+      this,
+      "ImportProductsFileFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "importProductsFile.importProductsFile",
+        code: lambda.Code.fromAsset("dist/handlers"),
+        role: lambdaRole,
+        environment: {
+          BUCKET_NAME: bucket.bucketName,
+        },
+      }
+    );
+
+    const api = new apigateway.RestApi(this, "ImportApi", {
+      restApiName: "Import Service",
+      description: "API for importing products",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
+    });
+
+    const productsResource = api.root.addResource("import");
+    productsResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFile)
+    );
+
+    new cdk.CfnOutput(this, "ImportApiUrl", {
+      value: api.url,
+    });
   }
 }
